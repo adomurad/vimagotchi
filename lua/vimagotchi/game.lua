16bf9492ad2aa96be8e83e95163c1de3d9086daf
Artur Domurad <domurad.artur@gmail.com>
2026-03-22 10:16:49 +0100

    refactor

--- lua/vimagotchi/game.lua
+++ lua/vimagotchi/game.lua
@@ -1,249 +1,150 @@
 local M = {}
 
+local creature = require("vimagotchi.creature")
+
 M._state = {
-  timer = 0,
-  pos = {
-    x = 4,
-    y = 4,
-  },
-  frame = 1,
-  frame_eat = 1,
-  text_to_eat = '',
+	timer = 0,
+	pos = {
+		x = 4,
+		y = 4,
+	},
+	frame = 1,
+	frame_eat = 1,
+	text_to_eat = "",
 }
 
 M.height = 10
 M.width = 40
 
-local animation = {
-  char_width = 3,
-  char_height = 3,
-  frames = {
-    [[
-⣀⣀⣀
-⣯⣿⣽
-⠈⠁⠁
-  ]],
-    [[
-⣀⣀⣀
-⣯⣿⣽
-⠈⠈⠁
-  ]],
-    [[
-⣀⣀⣀
-⢯⣿⡽
-⠈⠤⠁
-  ]],
-    [[
-⣀⣀⣀
-⣯⣿⣽
-⠈⠈⠁
-  ]],
-  },
-}
-
-local animation = {
-  char_width = 11,
-  char_height = 7,
-  frames = {
-    [[
-⠀⠀⠀⢀⣀⣀⣀⠀⠀⠀⠀
-⠀⢠⡾⠿⡿⠿⡿⠿⣦⠀⠀
-⠀⣿⣜⣃⠠⠠⢀⣛⣼⡇⠀
-⢰⣿⠿⣷⣭⣭⣵⡿⢿⣷⠀
-⢨⣿⣦⣼⣿⣿⣿⣤⣾⣯⠀
-⠸⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀
-⠀⠘⠒⠊⠉⠉⠉⠒⠚⠀⠀
-  ]],
-    [[
-⠀⠀⠀⢀⣀⣀⣀⠀⠀⠀⠀
-⠀⢠⡾⠿⡿⠿⡿⠿⣦⠀⠀
-⠀⣿⣌⣁⠠⠠⢀⣉⣼⡇⠀
-⢰⣿⡿⢷⣭⣭⣵⠿⣿⣷⠀
-⢨⣿⣷⣤⣿⣿⣧⣴⣿⣯⠀
-⠸⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀
-⠀⠘⠒⠊⠉⠉⠉⠒⠚⠀⠀
-  ]],
-    [[
-⠀⠀⠀⢀⣀⣀⣀⠀⠀⠀⠀
-⠀⢠⡾⠿⡿⠿⡿⠿⣦⠀⠀
-⠀⣿⣜⣃⠠⠠⢀⣛⣼⡇⠀
-⢰⣿⡿⢷⣭⣭⣵⠿⣿⣷⠀
-⢨⣿⣷⣤⣿⣿⣧⣴⣿⣯⠀
-⠸⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀
-⠀⠘⠒⠊⠉⠉⠉⠒⠚⠀⠀
-  ]],
-    [[
-⠀⠀⠀⢀⣀⣀⣀⠀⠀⠀⠀
-⠀⢠⡾⠿⡿⠿⡿⠿⣦⠀⠀
-⠀⣿⣜⣃⠠⠠⢀⣛⣼⡇⠀
-⢰⣿⠿⣷⣭⣭⣵⡿⢿⣷⠀
-⢨⣿⣦⣼⣿⣿⣿⣤⣾⣯⠀
-⠸⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀
-⠀⠘⠒⠊⠉⠉⠉⠒⠚⠀⠀
-  ]],
-    [[
-⠀⠀⠀⢀⣀⣀⣀⠀⠀⠀⠀
-⠀⢠⡾⠿⡿⠿⡿⠿⣦⠀⠀
-⠀⣿⣜⣃⠠⠠⢀⣛⣼⡇⠀
-⢰⣿⠿⣷⣭⣭⣵⡿⢿⣷⠀
-⢨⣿⣦⣼⣿⣿⣿⣤⣾⣯⠀
-⠸⣿⠿⢿⣿⣿⣿⣿⣿⡿⠀
-⠀⠈⠉⠉⠉⠉⠉⠒⠚⠀⠀
-  ]],
-  },
-}
-
-local animation_eat = {
-  char_width = 11,
-  char_height = 7,
-  frames = {
-    [[
-⠀⠀⠀⢀⣀⣀⣀⠀⠀⠀⠀
-⠀⢠⢪⡍⢫⣩⠋⣭⢢⠀⠀
-⠀⣿⣤⡖⠁⠀⠑⣦⣼⡇⠀
-⢰⣿⠿⣿⣶⣶⣾⡿⢿⣷⠀
-⢨⣿⣦⣼⣿⣿⣿⣤⣾⣯⠀
-⠸⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀
-⠀⠘⠒⠊⠉⠉⠉⠒⠚⠀⠀
-  ]],
-    [[
-⠀⠀⠀⢀⣀⣀⣀⠀⠀⠀⠀
-⠀⢠⢪⡍⢫⣩⠋⣭⢢⠀⠀
-⠀⣿⣤⣶⠁⠀⢱⣦⣼⡇⠀
-⢰⣿⠿⣿⣷⣶⣿⡿⢿⣷⠀
-⢨⣿⣦⣼⣿⣿⣿⣤⣾⣯⠀
-⠸⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀
-⠀⠘⠒⠊⠉⠉⠉⠒⠚⠀⠀
-  ]],
-  },
-}
-
 function M.init()
-  -- nothing
+	-- nothing
 end
 
 local function stringToArray(str)
-  local t = {}
-  local i = 1
-  while i <= #str do
-    local b = str:byte(i)
-    local len = (b < 0x80) and 1 or (b < 0xE0) and 2 or (b < 0xF0) and 3 or 4
-    table.insert(t, str:sub(i, i + len - 1))
-    i = i + len
-  end
-  return t
+	local t = {}
+	local i = 1
+	while i <= #str do
+		local b = str:byte(i)
+		local len = (b < 0x80) and 1 or (b < 0xE0) and 2 or (b < 0xF0) and 3 or 4
+		table.insert(t, str:sub(i, i + len - 1))
+		i = i + len
+	end
+	return t
 end
 
-local function render_char_on_screen(character)
-  local char_lines = vim.split(character, '\n')
+local function render_char_on_screen(creature_frame)
+	local char_lines = vim.split(creature_frame, "\n")
 
-  local lines = {}
+	local lines = {}
 
-  local pos = M._state.pos
-  if pos.y > 1 then
-    for y = 1, pos.y - 1 do
-      -- lines[y] = padding .. char_lines[]
-      lines[y] = ''
-    end
-  end
+	local pos = M._state.pos
+	if pos.y > 1 then
+		for y = 1, pos.y - 1 do
+			-- lines[y] = padding .. char_lines[]
+			lines[y] = ""
+		end
+	end
 
-  for y = pos.y, pos.y + animation.char_height do
-    local padding = string.rep(' ', pos.x - 1)
-    lines[y] = padding .. char_lines[y + 1 - pos.y]
+	for y = pos.y, pos.y + creature.char_height do
+		local padding = string.rep(" ", pos.x - 1)
+		lines[y] = padding .. char_lines[y + 1 - pos.y]
 
-    if #M._state.text_to_eat > 0 and y == pos.y + 2 then
-      -- local chars = vim.split(lines[y], '', { plain = true, trimempty = false })
-      local chars = stringToArray(lines[y])
-      local head, tail = vim.list_slice(chars, 1, #chars - 6), vim.list_slice(chars, #chars - 5)
-      -- print(vim.inspect(head))
-      -- -- print(vim.inspect(head))
-      lines[y] = table.concat(head)
-      -- print(lines[y])
+		if #M._state.text_to_eat > 0 and y == pos.y + 2 then
+			-- local chars = vim.split(lines[y], '', { plain = true, trimempty = false })
+			local chars = stringToArray(lines[y])
+			local head, tail = vim.list_slice(chars, 1, #chars - 6), vim.list_slice(chars, #chars - 5)
+			-- print(vim.inspect(head))
+			-- -- print(vim.inspect(head))
+			lines[y] = table.concat(head)
+			-- print(lines[y])
 
-      local eat_chars = stringToArray(M._state.text_to_eat)
+			local eat_chars = stringToArray(M._state.text_to_eat)
 
-      for char_index, char in ipairs(tail) do
-        local eat_char = vim.list_slice(eat_chars, char_index, char_index + 1)
+			for char_index, char in ipairs(tail) do
+				local eat_char = vim.list_slice(eat_chars, char_index, char_index + 1)
 
-        if #eat_char == 0 or eat_char[1] == ' ' then
-          lines[y] = lines[y] .. char
-        else
-          lines[y] = lines[y] .. eat_char[1]
-        end
-      end
+				if #eat_char == 0 or eat_char[1] == " " then
+					lines[y] = lines[y] .. char
+				else
+					lines[y] = lines[y] .. eat_char[1]
+				end
+			end
 
-      lines[y] = lines[y] .. table.concat(vim.list_slice(eat_chars, #tail + 1))
+			lines[y] = lines[y] .. table.concat(vim.list_slice(eat_chars, #tail + 1))
 
-      -- lines[y] = lines[y]:sub(1, -13)
-      -- lines[y] = lines[y] .. M._state.text_to_eat
-      M._state.text_to_eat = M._state.text_to_eat:sub(2)
-    end
-  end
+			-- lines[y] = lines[y]:sub(1, -13)
+			-- lines[y] = lines[y] .. M._state.text_to_eat
+			M._state.text_to_eat = M._state.text_to_eat:sub(2)
+		end
+	end
 
-  return lines
+	return lines
 end
 
 function M.next_frame()
-  M._state.timer = M._state.timer + 1
+	M._state.timer = M._state.timer + 1
 
-  local frameNr = M._state.frame
-  local frame = animation.frames[frameNr]
+	local frameNr = M._state.frame
+	local animation_idle = creature.animations.idle
+	local frame = animation_idle.frames[frameNr]
 
-  if #M._state.text_to_eat > 0 then
-    if M._state.timer % 2 == 0 then
-      M._state.frame_eat = M._state.frame_eat + 1
-    end
+	if #M._state.text_to_eat > 0 then
+		if M._state.timer % 2 == 0 then
+			M._state.frame_eat = M._state.frame_eat + 1
+		end
 
-    if M._state.frame_eat > #animation_eat.frames then
-      M._state.frame_eat = 1
-    end
+		local animation_eat = creature.animations.eat
 
-    local eat_frame = animation_eat.frames[M._state.frame_eat]
+		if M._state.frame_eat > #animation_eat.frames then
+			M._state.frame_eat = 1
+		end
 
-    return render_char_on_screen(eat_frame)
-  end
+		local eat_frame = animation_eat.frames[M._state.frame_eat]
 
-  if M._state.timer % 5 ~= 0 then
-    return render_char_on_screen(frame)
-  end
+		return render_char_on_screen(eat_frame)
+	end
 
-  frameNr = frameNr + 1
-  if frameNr > #animation.frames then
-    frameNr = 1
-  end
+	if M._state.timer % 5 ~= 0 then
+		return render_char_on_screen(frame)
+	end
 
-  M._state.frame = frameNr
+	frameNr = frameNr + 1
+	if frameNr > #animation_idle.frames then
+		frameNr = 1
+	end
 
-  -- move
-  local pos = M._state.pos
-  local rand_dir = math.random(0, 5)
-  if rand_dir == 0 then
-    pos.x = pos.x + 1
-  elseif rand_dir == 1 then
-    pos.x = pos.x - 1
-  elseif rand_dir == 2 then
-    pos.y = pos.y + 1
-  elseif rand_dir == 3 then
-    pos.y = pos.y - 1
-  end
+	M._state.frame = frameNr
 
-  if pos.x > M.width - animation.char_width + 1 then
-    pos.x = M.width - animation.char_width + 1
-  elseif pos.x < 1 then
-    pos.x = 1
-  elseif pos.y > M.height - animation.char_height + 1 then
-    pos.y = M.height - animation.char_height + 1
-  elseif pos.y < 1 then
-    pos.y = 1
-  end
+	-- move
+	local pos = M._state.pos
+	local rand_dir = math.random(0, 5)
+	if rand_dir == 0 then
+		pos.x = pos.x + 1
+	elseif rand_dir == 1 then
+		pos.x = pos.x - 1
+	elseif rand_dir == 2 then
+		pos.y = pos.y + 1
+	elseif rand_dir == 3 then
+		pos.y = pos.y - 1
+	end
 
-  return render_char_on_screen(frame)
+	if pos.x > M.width - animation_idle.char_width + 1 then
+		pos.x = M.width - animation_idle.char_width + 1
+	elseif pos.x < 1 then
+		pos.x = 1
+	elseif pos.y > M.height - animation_idle.char_height + 1 then
+		pos.y = M.height - animation_idle.char_height + 1
+	elseif pos.y < 1 then
+		pos.y = 1
+	end
+
+	return render_char_on_screen(frame)
 end
 
 function M.eat_text(text)
-  local padding_chars = M.width - M._state.pos.x - 8 - #M._state.text_to_eat
-  local padding = string.rep(' ', padding_chars)
-  M._state.text_to_eat = M._state.text_to_eat .. padding .. text
+	local padding_chars = M.width - M._state.pos.x - 8 - #M._state.text_to_eat
+	local padding = string.rep(" ", padding_chars)
+	M._state.text_to_eat = M._state.text_to_eat .. padding .. text
 end
 
 return M
--- lua/vimagotchi/init.lua
+++ lua/vimagotchi/init.lua
@@ -1,102 +1,90 @@
 local M = {}
 
-local Game = require 'vimagotchi.game'
+local Game = require("vimagotchi.game")
 Game.init()
 
 M._store = {
-  game_buf = nil,
+	game_buf = nil,
 }
 
-local valid_args = { 'Open' }
+local valid_args = { "Open" }
 
 local function complete_my_cmd(arg_lead, cmd_line, cursor_pos)
-  local matches = {}
-  for _, arg in ipairs(valid_args) do
-    if arg:find('^' .. arg_lead) then
-      table.insert(matches, arg)
-    end
-  end
-  return matches
+	local matches = {}
+	for _, arg in ipairs(valid_args) do
+		if arg:find("^" .. arg_lead) then
+			table.insert(matches, arg)
+		end
+	end
+	return matches
 end
 
 local function create_user_commands()
-  vim.api.nvim_create_user_command('Vimagotchi', function(args)
-    if args.args == '' then
-      print 'Error: MyCmd requires an argument (a, b, or c)'
-      return
-    end
+	vim.api.nvim_create_user_command("Vimagotchi", function(args)
+		if args.args == "" then
+			print("Error: Vimagotchi requires an argument (Open, )")
+			return
+		end
 
-    if args.args == 'Open' then
-      M.toggle_chat()
-    elseif args.args == 'ChatNew' then
-      M.new_chat()
-    elseif args.args == 'PasteIntoChat' then
-      M.paste_into_chat()
-    elseif args.args == 'PasteIntoNewChat' then
-      M.paste_into_new_chat()
-    elseif args.args == 'ChatSend' then
-      M.send_chat()
-    elseif args.args == 'ChatFind' then
-      M.find_chat()
-    elseif args.args == 'InlineAdd' then
-      M.inline_add()
-    elseif args.args == 'InlineEdit' then
-      M.inline_edit()
-    else
-      print 'Error: wrong arg!'
-    end
-  end, {
-    desc = 'Vimagotchi',
-    nargs = 1, -- Expect exactly one argument
-    complete = complete_my_cmd, -- Autocompletion function
-  })
+		if args.args == "Open" then
+			M.toggle_chat()
+		elseif args.args == "Close" then
+			-- M.new_chat()
+		else
+			print("Error: wrong arg!")
+		end
+	end, {
+		desc = "Vimagotchi",
+		nargs = 1, -- Expect exactly one argument
+		complete = complete_my_cmd, -- Autocompletion function
+	})
 end
 
 function M.setup()
-  create_user_commands()
-  M._store.game_buf = vim.api.nvim_create_buf(false, true)
+	create_user_commands()
+	M._store.game_buf = vim.api.nvim_create_buf(false, true)
 
-  local timer = vim.uv.new_timer()
-  if timer == nil then
-    error 'could not start a timer'
-    return
-  end
+	local timer = vim.uv.new_timer()
+	if timer == nil then
+		error("could not start a timer")
+		return
+	end
 
-  timer:start(
-    10,
-    100,
-    vim.schedule_wrap(function()
-      -- your animation frame update here
-      local frame = Game.next_frame()
-      vim.api.nvim_buf_set_lines(M._store.game_buf, 0, -1, true, frame)
-      -- vim.api.nvim_buf_set_lines(M._store.game_buf, 0, -1, true, vim.split(frame, '\n'))
-    end)
-  )
+	timer:start(
+		10,
+		100,
+		vim.schedule_wrap(function()
+			-- your animation frame update here
+			local frame = Game.next_frame()
+			vim.api.nvim_buf_set_lines(M._store.game_buf, 0, -1, true, frame)
+			-- vim.api.nvim_buf_set_lines(M._store.game_buf, 0, -1, true, vim.split(frame, '\n'))
+		end)
+	)
 
-  M.open()
+	M.open()
 
-  vim.api.nvim_create_autocmd('TextYankPost', {
-    callback = function(ev)
-      if vim.v.event.operator:match '[ydc]' then
-        local content = vim.fn.getreg(vim.v.event.regname or '"')
-        -- print('Deleted/yanked:\n' .. content)
-        Game.eat_text(content:gsub('[\r\n]+', ''))
-      end
-    end,
-  })
+	vim.api.nvim_create_autocmd("TextYankPost", {
+		callback = function(ev)
+			if vim.v.event.operator:match("[ydc]") then
+				local content = vim.fn.getreg(vim.v.event.regname or '"')
+				-- print('Deleted/yanked:\n' .. content)
+				Game.eat_text(content:gsub("[\r\n]+", ""))
+			end
+		end,
+	})
 end
 
 function M.open()
-  vim.api.nvim_open_win(M._store.game_buf, false, {
-    relative = 'editor',
-    width = Game.width,
-    height = Game.height,
-    col = vim.o.columns - Game.width - 2, -- 2 = small padding
-    row = vim.o.lines - Game.height - 3 - 1, -- status + cmdline + padding + lualine
-    style = 'minimal',
-    border = 'single', -- or 'rounded', 'none', etc.
-    focusable = false,
-  })
+	vim.api.nvim_open_win(M._store.game_buf, false, {
+		relative = "editor",
+		width = Game.width,
+		height = Game.height,
+		col = vim.o.columns - Game.width - 2, -- 2 = small padding
+		row = vim.o.lines - Game.height - 3 - 1, -- status + cmdline + padding + lualine
+		style = "minimal",
+		border = "single", -- or 'rounded', 'none', etc.
+		focusable = false,
+	})
 end
 
 return M
